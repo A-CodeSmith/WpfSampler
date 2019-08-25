28d1f763b971f00893989ad95778d12581943e3a
A-CodeSmith <sylace@gmail.com>
2019-08-25 02:32:41 -0400

    Added MockVM for TextViewerControl
    Added a mock ViewModel for the TextViewerControl.  This allows for a better view at Design Time.

---
+++ WpfSampler/Views/Controls/TextViewerControlMockVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfSampler.Views.Controls
+{
+    public class TextViewerControlMockVM
+    {
+        public string Label { get; set; } = "Text Label:";
+
+        public string Text { get; set; } = "<TIMESTAMP> DEBUG This is the first entry.\r\n"
+            + "<TIMESTAMP> DEBUG This is the second entry.\r\n"
+            + "<TIMESTAMP> ERROR This is an error.\r\n";
+    }
+}
